f2ac9f71d8082a00a8d6041f0d768bd53b1e0671
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-01-31 15:38:13 -0300

    responsive small adjustment in premium page

--- src/components/Premium.vue
+++ src/components/Premium.vue
@@ -14,7 +14,7 @@
     </ul>
   </div>
 
-  <div class="flex pt-14 gap-8 px-72 items-center max-md:flex-col max-md:p-0 max-md:pt-12 max-lg:flex-col max-lg:px-36 max-lg:items-center max-xl:px-12">
+  <div class="flex justify-center pt-14 gap-8 px-72 items-center max-md:flex-col max-md:p-0 max-md:pt-12 max-lg:flex-col max-lg:px-36 max-lg:items-center max-xl:px-12 max-lg:text-center">
 
     <div class="bg-white p-4 flex flex-col justify-between card-height shadow-2xl rounded-md max-md:my-0 max-md:mx-auto max-md:justify-start max-md:px-4 max-md:py-8 max-lg:w-96">
       <div>
@@ -95,38 +95,7 @@
           available for users who have already tried Premium.</p>
       </div>
     </div>
-
-     <div class="bg-white p-4 flex flex-col justify-between card-height shadow-2xl rounded-md max-md:my-0 max-md:mx-auto max-md:justify-start max-md:px-4 max-md:py-8 max-lg:w-96">
-      <div>
-        <span class="bg-blue-600 text-white font-bold text-sm py-1 px-4 rounded-md">2 months free</span>
-        <h3 class="text-2xl font-bold max-md:pt-2">Student</h3>
-        <p class="max-md:pt-2 max-md:font-semibold max-md:text-lg">$4.99/month after offer period 1 account</p>
-        <p class="text-white">__________________________________</p>
-
-        <ul class="flex flex-col gap-0 border-slate-400 border-t-2">
-          <li class="pt-4 flex gap-2 max-md:font-semibold"> <span>✓</span>
-            <p> Hulu (With Ads) plan </p>
-          </li>
-          <li class="pt-4 flex gap-2 max-md:font-semibold"> <span>✓</span>
-            <p> SHOWTIME </p>
-          </li>
-          <li class="pt-4 flex gap-2 max-md:font-semibold"> <span>✓</span>
-            <p> Ad-free music listening </p>
-          </li>
-          <li class="pt-4 flex gap-2 max-md:font-semibold"> <span>✓</span>
-            <p> Play anywhere - even offline </p>
-          </li>
     
-
-        </ul>
-      </div>
-      <div class="flex flex-col justify-end max-md:pt-8">
-        <button class="bg-black text-white font-bold uppercase py-3 px-10 rounded-full text-sm">Get Started</button>
-        <p class="text-small pt-4 pb-10 max-md:p-0 max-md:pt-4"> <a href="" class="underline">Terms and conditions apply.</a> 1 month free not
-          available for users who have already tried Premium.</p>
-      </div>
-    </div>
-
   </div>
 
 </div>
