6f681cb881d1a908dc77eb4b3a7155ea6429dbfe
Jeffs-Soares <soares.jeffersonthadeu@gmail.com>
2024-01-31 12:23:09 -0300

    migrated

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import BannerVue from "./components/Banner.vue";
+import FooterVue from "./components/Footer.vue";
+import HeaderVue from "./components/Header.vue";
+import PremiumVue from "./components/Premium.vue";
+import WhyPremiumVue from "./components/WhyPremium.vue";
+
+
+</script>
+
+<template >
+
+  <HeaderVue />
+  <BannerVue />
+  <PremiumVue />
+  <WhyPremiumVue/>
+  <FooterVue />
+ 
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="bg-black">
+  <div class="max-w-7xl mx-auto my-0">
+    <nav class="p-4 flex justify-between items-center text-white">
+      <div>
+        <img class="w-32 inline-block max-md:w-24" src="../assets/img/spotifyLogo.png" alt="Logo">
+      </div>
+
+      <ul class="flex gap-8 text-sm font-bold">
+        <li><a class="hover:text-green-400 transition-all duration-200" href="">Premium</a></li>
+        <li><a class="hover:text-green-400 transition-all duration-200" href="">Support</a></li>
+        <li><a class="hover:text-green-400 transition-all duration-200" href="">Download</a></li>
+        <li><span>|</span></li>
+        <li><a class="hover:text-green-400 transition-all duration-200" href="">Sing up</a></li>
+        <li><a class="hover:text-green-400 transition-all duration-200" href="">Log in</a></li>
+
+
+      </ul>
+    </nav>
+  </div>
+
+</div>
+</template>
+
+<script>
+  
+</script>
+
+<style scoped>
+
+</style>
